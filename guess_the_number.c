c96b9b3bd673376986117718a651d13e9e7f7b48
Mas Renko <masrenko123@gmail.com>
2021-10-04 11:25:22 +0545

    first commit

---
+++ guess_the_number.c
@@ -0,0 +1,26 @@
+// Guess The Number 
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int number, guess, nguesses=1;
+    srand(time(0));
+    number = rand()%100 + 1; // Generates a random number between 1 and 100
+    do{  // loop until the number is guessed
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d", &guess);
+        if(guess>number){
+            printf("Press Lower number\n");
+        }
+        else if(guess<number){
+            printf("Press Higher number\n");
+        }
+        else{
+            printf("You guessed it in %d attempts\n", nguesses);
+        }
+        nguesses++;
+    }while(guess!=number);
+    
+    return 0;
+}
